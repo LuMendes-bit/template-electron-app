893dc6466a0f0048697a5a8f8af84a9ed0daf1e5
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-05-13 20:27:46 -0300

    Estrutura de arquivos

---
+++ main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron');
+function createwindow() {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+window.loadFile('src/pages/index.html');
+}
+app.whenReady().then( () => {
+    createwindow();
+} )
